b9f7933514351a1d1f918a7dcc50b4d4e9480e3c
David-49 <59173768+David-49@users.noreply.github.com>
2020-08-07 03:12:38 +0200

    configuration gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,25 @@
-const { src, dest, series, watch, parallel } = require("gulp");
+const { src, dest, series } = require("gulp");
 const sass = require("gulp-sass");
 const del = require("del");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const sourcemaps = require("gulp-sourcemaps");
 
 function processSass() {
-  return src("sass/*.scss", { sourcemaps: true })
+  return src(["sass/*.scss", "!sass/breakpoints.scss"])
+    .pipe(sourcemaps.init())
     .pipe(sass().on("error", sass.logError))
-    .pipe(dest("css", { sourcemaps: "." }));
+    .pipe(dest("css"))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(sourcemaps.write())
+    .pipe(dest("css"));
 }
 
 function clean() {
-  return del(["*.png", "*.jpg"]);
+  return del(["css"]);
 }
 
-/*exports.default = series(clean, processSass);*/
-exports.default = function () {
-  watch("sass/*.scss", series(clean, processSass));
-};
+exports.default = series(clean, processSass);
+/* exports.default = function () {
+  watch("sass/*.scss", processSass);
+}; */
