f84b7e782fb99854d97f0df8823c83d36466a99b
David-49 <59173768+David-49@users.noreply.github.com>
2020-08-07 16:26:06 +0200

    début responsive

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,31 @@
-const { src, dest, series, parallel } = require("gulp");
+const { src, dest, series, parallel, watch } = require("gulp");
 const sass = require("gulp-sass");
 const del = require("del");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const cssnano = require("cssnano");
 const rename = require("gulp-rename");
+const newer = require("gulp-newer");
 //const sourcemaps = require("gulp-sourcemaps");
+
+const imgDest = "dist/images";
 
 function processHTML() {
   return src("src/*.html").pipe(dest("dist"));
 }
 
 function processIMG() {
-  return src("src/images/*.png").pipe(dest("dist/images"));
+  return src("src/images/*.png").pipe(newer(imgDest)).pipe(dest("dist/images"));
 }
 
 function processSass() {
-  return src(["src/sass/*.scss"])
+  return src(["src/sass/*.scss", "!src/sass/breakpoints.scss"])
     .pipe(sass().on("error", sass.logError))
-    .pipe(dest("dist/css"))
+    .pipe(dest("src/css"));
+}
+
+function processMinify() {
+  return src("src/css/*.css")
     .pipe(postcss([autoprefixer(), cssnano()]))
     .pipe(
       rename({
@@ -32,7 +39,12 @@ function clean() {
   return del(["dist"]);
 }
 
-exports.default = series(clean, parallel(processHTML, processIMG, processSass));
-/* exports.default = function () {
-  watch("sass/*.scss", processSass);
-}; */
+/*exports.default = series(
+  clean,
+  processSass,
+  parallel(processHTML, processIMG, processMinify)
+);*/
+
+exports.default = function () {
+  watch("src/sass/*.scss", processSass);
+};
