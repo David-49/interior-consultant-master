fcaba041013b57fc27ca3024fa7f42e4cf1f3eeb
David-49 <59173768+David-49@users.noreply.github.com>
2020-08-07 13:55:29 +0200

    Config gulp v2

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,38 @@
-const { src, dest, series } = require("gulp");
+const { src, dest, series, parallel } = require("gulp");
 const sass = require("gulp-sass");
 const del = require("del");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
-const sourcemaps = require("gulp-sourcemaps");
+const cssnano = require("cssnano");
+const rename = require("gulp-rename");
+//const sourcemaps = require("gulp-sourcemaps");
+
+function processHTML() {
+  return src("src/*.html").pipe(dest("dist"));
+}
+
+function processIMG() {
+  return src("src/images/*.png").pipe(dest("dist/images"));
+}
 
 function processSass() {
-  return src(["sass/*.scss", "!sass/breakpoints.scss"])
-    .pipe(sourcemaps.init())
+  return src(["src/sass/*.scss"])
     .pipe(sass().on("error", sass.logError))
-    .pipe(dest("css"))
-    .pipe(postcss([autoprefixer()]))
-    .pipe(sourcemaps.write())
-    .pipe(dest("css"));
+    .pipe(dest("dist/css"))
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(
+      rename({
+        suffix: ".min",
+      })
+    )
+    .pipe(dest("dist/css"));
 }
 
 function clean() {
-  return del(["css"]);
+  return del(["dist"]);
 }
 
-exports.default = series(clean, processSass);
+exports.default = series(clean, parallel(processHTML, processIMG, processSass));
 /* exports.default = function () {
   watch("sass/*.scss", processSass);
 }; */
